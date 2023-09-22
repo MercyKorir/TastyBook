8391412fbbf657922dfff8d3cef180dac4b38b0c
MercyKorir <mcthegreat78@gmail.com>
2023-09-22 13:54:15 +0300

    feat: session handler on launch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import SignupForm from "./components/SignupForm";
 import UserProfile from "./components/UserProfile";
 import AuthWrapper from "./components/AuthWrapper";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  const [cookies, ,] = useCookies(["token"]);
+
+  useEffect(() => {
+    if (cookies.token) {
+      navigate("/home");
+    } else {
+      navigate("/");
+    }
+  }, [cookies.token, navigate]);
+
   return (
     <div>
       <Header />
