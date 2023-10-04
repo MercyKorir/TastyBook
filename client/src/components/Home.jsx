6dea9e32f84f02a859e696a7b85dd2ff4dee0c6e
Minervus00 <fayouedraogo@protonmail.com>
2023-10-04 14:29:34 +0000

    home page

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+
+	return (
+		<div classsName="container">
+			<div className="left">
+				<h2> Find your favorite recipes </h2>
+				<h5>
+					The TastyBook is a cooking app that offers avariety of recipes
+					coming from all around the world, suitable for all cooking skill levels.
+					Recipes are created by cooking enthusiasts and shared with detailed
+					instructions, ingredients and all tools needed for an exceptional
+					cooking experience. Join our community of foodies and explore a world of
+					delicious flavors with TastyBook.
+				</h5>
+				<button> Take a Look! </button>
+			</div>
+			<div className="right">
+				<p> Lorem </p>
+				<p> Ipsum </p>
+				<p> Fidelis </p>
+				<p> Octum </p>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
